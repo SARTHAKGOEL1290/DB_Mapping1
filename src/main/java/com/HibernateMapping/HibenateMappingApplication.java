af452b62128969fd806f550ea02e43a6e6bf26d7
Sarthak Goel <sarthak.goel2@globallogic.com>
2022-02-21 18:26:03 +0530

    first message

---
+++ src/main/java/com/HibernateMapping/HibenateMappingApplication.java
@@ -0,0 +1,13 @@
+package com.HibernateMapping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibenateMappingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibenateMappingApplication.class, args);
+	}
+
+}
